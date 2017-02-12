16ed392a3e6ba3d82ebae8bfb9c8a73dbd1cd0f9
zlevi91 <zlevi91@gmail.com>
2017-02-12 15:55:34 +0200

    local inner class

--- Lesson06-Nested Classes/src/com/company/Main.java
+++ Lesson06-Nested Classes/src/com/company/Main.java
@@ -9,5 +9,27 @@ public class Main {
         Circle.Point point= new Circle().new Point(4,5);//חייבים ליצור אובייקא מסוג סירקל כדי שנוכל ליצור את הפויינט, מחלקה מקוננת פנימית
         Circle c= new Circle();
         Circle.Point point1= c.new Point(7,8);
+        Circle.Point point2=c.new Point(87,32);
+        c.setCenter(point1);
+
+    }
+    //אילוף כלבים
+    static Dog trainDog(Dog d){
+        class TrainedDog extends Dog{
+            @Override
+            void bark() {
+                System.out.println("abcdefghijklmnop...");
+            }
+        }
+        TrainedDog trainedDog=new TrainedDog();
+        trainedDog.name=d.name;
+        return trainedDog;
+    }
+}
+
+class Dog{
+    String name;
+    void bark(){
+        System.out.println("waf waf..." +name);
     }
 }
