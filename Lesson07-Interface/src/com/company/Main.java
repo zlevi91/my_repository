dc2da9a046f6a8518b94201e6c6504b19441b7eb
zlevi91 <zlevi91@gmail.com>
2017-02-13 14:34:23 +0200

    קומפרבל

--- Lesson07-Interface/src/com/company/Main.java
+++ Lesson07-Interface/src/com/company/Main.java
@@ -12,6 +12,24 @@ public class Main {
         Drawable[] drawables;
 
     }
+
+    //מיון בועות- משווה כל נתון עם הנתון שלידו ואם השני קטן יותר אז מחליפים בינהם
+    static void bubbleSort(int[] arr){
+        int upTo=arr.length-1;
+        boolean isSorted=false;
+        while(!isSorted){
+            isSorted=true;
+            for (int i = 0; i <upTo ; i++) {
+                if(arr[i]>arr[i+1]){
+                    int temp=arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=temp;
+                    isSorted=false;//אם יש פעם שהוא לא נכנס לעשות שום החלפה אז ז"א שהוא סיים את המיון (ולא סומן הדגל בשקר)
+                }
+            }
+            upTo--;//  פחות האיבר האחרון כיון שבכל איטרציה האיבר האחרון הוא האיבר שנכנס למקום שלו
+        }
+    }
 }
 interface Rollable{
     void roll(float degree);
@@ -61,5 +79,14 @@ class Cylinder extends Shape implements Rollable{
     public void roll(float degree) {
 
     }
+}
+interface Edible{
+    void eat (String foo);
+}
+//אינטרפייס שגאווה כתבו
+
+interface Comparable{
+    public int compareTo(Object obj);//למה התכוון המשורר במתודה הזאת? התכוון שיוחזר מהמתודה מספר 1,0,-1
+    // אם האובייקט שעליו הופעלה המתודה יותר גדול יוחזר 1 אם שווים יוחזר 0 ואם הוא קטן יוחזר -1
 }
 
