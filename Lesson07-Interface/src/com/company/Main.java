4f4b11b442827421ca14a9ea48d8524e7a2953e4
zlevi91 <zlevi91@gmail.com>
2017-02-13 15:58:31 +0200

    השוואה של פוינטים

--- Lesson07-Interface/src/com/company/Main.java
+++ Lesson07-Interface/src/com/company/Main.java
@@ -13,17 +13,31 @@ public class Main {
         rollable.roll(13);//אם נעשה לו דאוןקאסט לבול נוכל להפעיל את המתודה באונס בלי זה אי אפשר להפעיל את המתודה
         Drawable[] drawables;
 
+        Point[] myPoint={
+                new Point(4,5),
+                new Point(1,2),
+                new Point(7,1),
+                new Point(98,22),
+                new Point(2,7),//אותו מרחק כמו ה7,2 אבל פה הזוית יותר גדולה
+                new Point(7,2)
+        };
+        bubbleSort(myPoint);//ממיין את המערך
+
+        for (int i = 0; i <myPoint.length ; i++) {
+            System.out.print(myPoint[i]);
+        }
+
     }
 
     //מיון בועות- משווה כל נתון עם הנתון שלידו ואם השני קטן יותר אז מחליפים בינהם
-    static void bubbleSort(int[] arr){
+    static void bubbleSort(Comparable[] arr){
         int upTo=arr.length-1;
         boolean isSorted=false;
         while(!isSorted){
             isSorted=true;
             for (int i = 0; i <upTo ; i++) {
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
+                if(arr[i].compareTo(arr[i+1])>0){//גדול מ0 כי אמור להחזיר חיובי שלילי או 0
+                    Comparable temp=arr[i];
                     arr[i]=arr[i+1];
                     arr[i+1]=temp;
                     isSorted=false;//אם יש פעם שהוא לא נכנס לעשות שום החלפה אז ז"א שהוא סיים את המיון (ולא סומן הדגל בשקר)
@@ -95,6 +109,17 @@ interface Edible{
 class Point implements Comparable{
     int x,y;
 
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "("+x+","+y+")";
+    }
+
+    //מכריע מי יותר גדול בין 2 נקודות
     @Override
     public int compareTo(Object o) {
         if(o== null || !(o instanceof Point))
@@ -109,15 +134,28 @@ class Point implements Comparable{
         else if(d1<d2)
             return -1;
         if(this.x>=0){
-            if(other.x>=0) {//אם שני האיקסים חיוביים
-                if (this.y >= 0 && other.y >= 0) {//וגם שני הוויים חיוביים
+            if(other.x>=0) {//אם שני האיקסים חיוביים ז"א ברביע הראשון או הרביעי
+                if (this.y * other.y >= 0) {//וגם שני הוויים חיוביים או שניהם שליליים כי זווית יותר גדולה זה זה עם הוואי הגדול יותר
                     if (this.y >= other.y)
                         return 1;
                     else
                         return -1;
-
+               }else{//אם הם שוני סימן.. שניהם בצד הימני
+                    if(this.y<0)//אז הדיס נמצא ברביע הרביעי ואזר ברביע הראשון אז זה שברביע הרביעי הזווית יותר גדולה
+                        return 1;
+                    else
+                        return -1;
                 }
+            }else{//אם אזר נקודה איקס שלילי ואזר נמצא ברביע ...
+                if (this.y>=0)
+                    return -1;
+                else
+                    return 1;
+
             }
+        }else {
+            //כל מה שעשינו זה תחת העולם של דיס נקודה איקס גדול מ0
+            //מה שנשאר לכתוב זה שדיס נקודה איקס קטן מ0 רביע שלישי ורביעי
         }
         return 0;
     }
